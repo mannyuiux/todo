b20706bbfbc10287d991698548684225a085e106
Subramanian <Subramanian.m@unilogcorp.com>
2019-06-06 12:20:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import DayPicker from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ToDo />
   );
+}
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      date: '',
+    }
+    this.handleToDoMessage = this.handleToDoMessage.bind(this);
+    this.handleToDoDate = this.handleToDoDate.bind(this);
+  }
+  handleToDoMessage(e){
+    this.setState({
+      message: e.target.value
+    })
+  }
+  handleToDoDate(e){
+    this.setState({
+      date: e.target.value
+    })
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="addToDo">
+          <div className="formGroup">
+            <input type="text" placeholder="Think. Update. Execute" onChange={this.handleToDoMessage} />
+          </div>
+          <div className="formGroup">
+          <DayPicker onClick={this.handleToDoDate} />
+          </div>
+        </div>
+        <ul className="ToDoList">
+          {}
+        </ul>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
