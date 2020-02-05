76eb505f6e5498ab16e525f320fe7d6f870ed95a
mannyuiux <mannyuiux@gmail.com>
2020-02-06 03:12:54 +0530

    backend configuration and api route creationm

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,77 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const todoRoutes = express.Router();
 const PORT = 4000;
+
+let Todo = require("./todos.model");
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/todos", todoRoutes);
+
+//to connect with DB
+mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once("open", function() {
+  console.log("MongoDB database successfully connected");
+});
 
 app.listen(PORT, function() {
-    console.log("Server is running on Port: " + PORT);
+  console.log("Server is running on Port: " + PORT);
+});
+
+todoRoutes.route("/").get(function(req, res) {
+  Todo.find(function(err, todos) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todos);
+    }
+  });
+});
+
+todoRoutes.route("/:id").get(function(req, res) {
+  let id = req.params.id;
+  Todo.findById(id, function(err, todo) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todo);
+    }
+  });
+});
+
+todoRoutes.route("/add").post(function(req, res) {
+  let todo = new Todo(req.body);
+  todo
+    .save()
+    .then(todo => {
+      res.status(200).json({ todo: "todo added successfully" });
+    })
+    .catch(err => {
+      res.status(400).send("adding new todo failed");
+    });
+});
+
+todoRoutes.route("/update/:id").post(function(req, res) {
+  Todo.findById(req.params.id, function(err, todo) {
+    if (!todo) res.status(404).send("data is not found");
+    else todo.todo_desc = req.body.todo_desc;
+    todo.todo_author = req.body.todo_author;
+    todo.todo_category = req.body.todo_category;
+    todo.todo_flag = req.body.todo_flag;
+
+    todo
+      .save()
+      .then(todo => {
+        res.json("Todo updated!");
+      })
+      .catch(err => {
+        res.status(400).send("Update not possible");
+      });
+  });
 });
---
+++ backend/todos.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//Defining Schema
+let Todo = new Schema({
+  todo_desc: {
+    type: String
+  },
+  todo_author: {
+    type: String
+  },
+  todo_category: {
+    type: String
+  },
+  todo_flag: {
+    type: Boolean
+  }
+});
+
+module.exports = mongoose.model("Todo", Todo);
